ffcde572151a4dbff8269604616b079d6d277c6f
Aditya Gaurav <aditya.smit@gmail.com>
2017-02-26 13:25:52 -0800

    updated FileToFileOperations.class

--- src/com/handsonjava/fileoperations/FileToFileOperations.java
+++ src/com/handsonjava/fileoperations/FileToFileOperations.java
@@ -16,13 +16,13 @@ public class FileToFileOperations {
 
     public static void writeIntoFile(String filePath) {
 
-        File file = new File(filePath);
+//        File file = new File(filePath);
         try {
 
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((System.in)));
             String consoleData = bufferedReader.readLine();
 
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(filePath),true));
             bufferedWriter.write(consoleData);
 
 //            PrintWriter out = new PrintWriter(new FileOutputStream(file));
@@ -42,7 +42,9 @@ public class FileToFileOperations {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(filePathToReadFrom)));
             String data = bufferedReader.readLine();
 
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(filePathToWrite)));
+//            while passing true as an argument, it does not overwrite data. It appends at the end of existing line.
+//            So if you remove true, it overwrites the existing line by the the new line.
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(filePathToWrite),true));
             bufferedWriter.write(data);
             System.out.println("=======Process Completed============");
 
@@ -56,7 +58,7 @@ public class FileToFileOperations {
     public static void main(String[] args) {
         String file = "/Users/adityag/Projects1/Mydetails1.txt";
         String file2 = "/Users/adityag/Projects/Mydetails.txt";
-//        writeIntoFile(file);
-        writeToFile(file, file2);
+        writeIntoFile(file);
+       writeToFile(file, file2);
     }
 }
